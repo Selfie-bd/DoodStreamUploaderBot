5fd4a636d61583a28d1a3639c325fad1640a5a0c
Selfie SD <webtamil0@gmail.com>
2022-02-03 21:59:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,13 +6,13 @@ from os import environ
 
 ### Getting ENvironment Variables
 class Config(object):
-    BOT_TOKEN = environ.get("BOT_TOKEN", "") # Make a bot from https://t.me/BotFather
+    BOT_TOKEN = environ.get("BOT_TOKEN", "5087927386:AAEx7kS8Rpj15H6xpXtyeW3VGerTucoj_mo") # Make a bot from https://t.me/BotFather
     
-    APP_ID = int(environ.get("API_ID", "0")) # Get this value from https://my.telegram.org/apps
+    APP_ID = int(environ.get("API_ID", "1976680")) # Get this value from https://my.telegram.org/apps
     
-    API_HASH = environ.get("API_HASH", "") # Get this value from https://my.telegram.org/apps
+    API_HASH = environ.get("API_HASH", "9073255ce64a6072a59099803493f97d") # Get this value from https://my.telegram.org/apps
     
-    OWNER_ID = int(environ.get("OWNER_ID", None)) # Your(owner's) telegram id
+    OWNER_ID = int(environ.get("OWNER_ID", "1940030638")) # Your(owner's) telegram id
     
-    MONGO_STR = environ.get("MONGO_STR", "") # Get from MongoDB Atlas
+    MONGO_STR = environ.get("MONGO_STR", "mongodb+srv://erichdaniken:erichdaniken@cluster0.c13qk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority") # Get from MongoDB Atlas
 

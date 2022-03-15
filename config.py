dc0fbd94e73494488fdc5608bdfbec955ba7bf97
Selfie SD <webtamil0@gmail.com>
2022-03-15 06:58:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,13 +6,13 @@ from os import environ
 
 ### Getting ENvironment Variables
 class Config(object):
-    BOT_TOKEN = environ.get("BOT_TOKEN", "5087927386:AAEx7kS8Rpj15H6xpXtyeW3VGerTucoj_mo") # Make a bot from https://t.me/BotFather
+    BOT_TOKEN = environ.get("BOT_TOKEN", "5194035154:AAE_IlC8CNAycbtFQpQx6z-Ys1eQMoyLsiU") # Make a bot from https://t.me/BotFather
     
-    APP_ID = int(environ.get("API_ID", "1976680")) # Get this value from https://my.telegram.org/apps
+    APP_ID = int(environ.get("API_ID", "2904321")) # Get this value from https://my.telegram.org/apps
     
-    API_HASH = environ.get("API_HASH", "9073255ce64a6072a59099803493f97d") # Get this value from https://my.telegram.org/apps
+    API_HASH = environ.get("API_HASH", "87faa32f68324a09aecdd9911db846a2") # Get this value from https://my.telegram.org/apps
     
-    OWNER_ID = int(environ.get("OWNER_ID", "1940030638")) # Your(owner's) telegram id
+    OWNER_ID = int(environ.get("OWNER_ID", "1906359620")) # Your(owner's) telegram id
     
-    MONGO_STR = environ.get("MONGO_STR", "mongodb+srv://erichdaniken:erichdaniken@cluster0.c13qk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority") # Get from MongoDB Atlas
+    MONGO_STR = environ.get("MONGO_STR", "mongodb+srv://erichdaniken:erichdaniken@cluster0.c13qk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 
